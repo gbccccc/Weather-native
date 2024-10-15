8b79390df7dd96e88e95200e8fa0cb085fb03d32
gbccccc <gbccccc233@gmail.com>
2024-10-14 18:09:11 -0700

    weather code mapping

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -108,3 +108,98 @@ const stateMapping = {
   "WI": "Wisconsin",
   "WY": "Wyoming"
 };
+
+const weatherMapping = {
+  1000: {
+    "description": "Clear",
+    "iconName": "clear_day.svg"
+  },
+  1100: {
+    "description": "Mostly Clear",
+    "iconName": "mostly_clear_day.svg"
+  },
+  1101: {
+    "description": "Partly Cloudy",
+    "iconName": "partly_cloudy_day.svg"
+  },
+  1102: {
+    "description": "Mostly Cloudy",
+    "iconName": "mostly_cloudy.svg"
+  },
+  1001: {
+    "description": "Cloudy",
+    "iconName": "cloudy.svg"
+  },
+  2100: {
+    "description": "Light Fog",
+    "iconName": "fog_light.svg"
+  },
+  2000: {
+    "description": "Fog",
+    "iconName": "fog.svg"
+  },
+  4000: {
+    "description": "Drizzle",
+    "iconName": "drizzle.svg"
+  },
+  4200: {
+    "description": "Light Rain",
+    "iconName": "rain_light.svg"
+  },
+  4001: {
+    "description": "Rain",
+    "iconName": "rain.svg"
+  },
+  4201: {
+    "description": "Heavy Rain",
+    "iconName": "rain_heavy.svg"
+  },
+  5001: {
+    "description": "Flurries",
+    "iconName": "flurries.svg"
+  },
+  5100: {
+    "description": "Light Snow",
+    "iconName": "snow_light.svg"
+  },
+  5000: {
+    "description": "Snow",
+    "iconName": "snow.svg"
+  },
+  5101: {
+    "description": "Heavy Snow",
+    "iconName": "snow_heavy.svg"
+  },
+  6000: {
+    "description": "Freezing Drizzle",
+    "iconName": "freezing_drizzle.svg"
+  },
+  6200: {
+    "description": "Light Freezing Drizzle",
+    "iconName": "freezing_rain_light.svg"
+  },
+  6001: {
+    "description": "Freezing Rain",
+    "iconName": "freezing_rain.svg"
+  },
+  6201: {
+    "description": "Heavy Freezing Rain",
+    "iconName": "freezing_rain_heavy.svg"
+  },
+  7102: {
+    "description": "Light Ice Pellets",
+    "iconName": "ice_pellets_light.svg"
+  },
+  7000: {
+    "description": "Ice Pellets",
+    "iconName": "ice_pellets.svg"
+  },
+  7101: {
+    "description": "Heavy Ice Pellets",
+    "iconName": "ice_pellets_heavy.svg"
+  },
+  8000: {
+    "description": "Thunderstorm",
+    "iconName": "tstorm.svg"
+  }
+}
